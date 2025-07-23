6c4ce94c4760109dcc77d028577a3d158feef961
Ying Chen <44314773+YingChen10@users.noreply.github.com>
2025-07-23 18:41:26 -0500

    Update Plot_liver_weight.R

--- Plot_liver_weight.R
+++ Plot_liver_weight.R
@@ -23,7 +23,7 @@ plot <- function(result, parameter, ylab){
   
   
   p1 <- ggplot() + 
-    geom_col(data = mean, aes(x = Condition, y = mean, fill = Condition), width = 0.5) +
+    geom_col(data = mean, aes(x = Condition, y = mean, fill = Condition), width = 0.3) +
     geom_errorbar(data = mean, aes(x = Condition, ymin = mean - sd/(5^0.5), ymax = mean + sd/(5^0.5), color = Condition), width = 0) +
     geom_jitter(data = tmp, aes(x = Condition, y = value), size = 0.8) + 
     theme + 
